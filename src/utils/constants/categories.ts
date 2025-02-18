f88b10962581728999f19e7c6274c066a5f4c1b4
Phua Si Yu Arix <arixgg@gmail.com>
2025-02-18 23:26:31 +0800

    wip: add Category enum

---
+++ src/utils/constants/categories.ts
@@ -0,0 +1,23 @@
+import { MdFastfood } from 'react-icons/md'
+
+export const Category = {
+  Entertainment: 'ENTERTAINMENT',
+  Fitness: 'FITNESS',
+  Food: 'FOOD',
+  Gifts: 'GIFTS',
+  Hobbies: 'HOBBIES',
+  Insurance: 'INSURANCE',
+  Medical: 'MEDICAL',
+  Pet: 'PET',
+  Shopping: 'SHOPPING',
+  Transport: 'TRANSPORT',
+  Travel: 'TRAVEL',
+  Utilities: 'UTILITIES',
+  Work: 'WORK',
+} as const
+
+export type Category = (typeof Category)[keyof typeof Category]
+
+export const CATEGORY_ICON = {
+  [Category.Food]: MdFastfood,
+}
