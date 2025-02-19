5680aa0b67a88734e7ff728455ff23263ccde9fa
arixpsy <arixgg@gmail.com>
2025-02-19 20:45:49 +0800

    tweak: add query client to root

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { ClerkProvider } from '@clerk/clerk-react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
@@ -11,19 +12,29 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env.local file')
 }
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Login />} />
-          <Route element={<ActionBarLayout />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="history" element={<div>history</div>} />
-            <Route path="wallets" element={<div>wallets</div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ClerkProvider>
+    <QueryClientProvider client={client}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Login />} />
+            <Route element={<ActionBarLayout />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="history" element={<div>history</div>} />
+              <Route path="wallets" element={<div>wallets</div>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ClerkProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
