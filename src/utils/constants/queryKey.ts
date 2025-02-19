c3700cc0426383d252492378aadfd967cf312b3e
arixpsy <arixgg@gmail.com>
2025-02-19 20:48:42 +0800

    magdagdag: querykeys for api fetches

---
+++ src/utils/constants/queryKey.ts
@@ -0,0 +1,7 @@
+const WHISCASH = 'whiscash'
+
+export const QUERY_KEYS = {
+  DASHBOARD: [WHISCASH, 'dashboard'],
+  WALLETS: [WHISCASH, 'wallets'],
+  TRANSACTIONS: [WHISCASH, 'transactions'],
+} as const
