12fe43aa66f3d07a9dbbfc71502e2e4f9f64d592
arixpsy <arixgg@gmail.com>
2025-02-21 12:59:05 +0800

    wip: extract <Modal />

--- src/components/commons/Modal/index.tsx
+++ src/components/commons/Modal/index.tsx
@@ -1,6 +1,9 @@
 import { motion } from 'motion/react'
+import { PropsWithChildren } from 'react'
 
-const Modal = () => {
+const Modal = (props: PropsWithChildren) => {
+  const { children } = props
+
   return (
     <div className="fixed inset-0 grid place-items-center [&>*]:z-0 [&>*]:col-start-1 [&>*]:row-start-1">
       <motion.div
@@ -10,26 +13,7 @@ const Modal = () => {
         animate={{ opacity: 0.5 }}
         exit={{ opacity: 0 }}
       />
-
-      {/* TODO: extract logic */}
-      <motion.div
-        className="h-full w-full max-w-md self-end rounded-t-2xl bg-white p-3"
-        initial={{ translateY: '100%' }}
-        animate={{ translateY: '0%' }}
-        exit={{ translateY: '100%' }}
-        transition={{ type: 'tween', ease: 'easeOut' }}
-      >
-        <div className="flex justify-between">
-          <div onClick={() => window.history.back()}>X</div>
-          <div>Create Wallet</div>
-        </div>
-        <div>
-          <p>Name</p>
-          <p>Currency</p>
-          <p>defaultSpendingPeriod</p>
-          <p>Sub Wallet</p>
-        </div>
-      </motion.div>
+      {children}
     </div>
   )
 }
