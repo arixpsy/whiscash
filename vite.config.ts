c0fcd7f489f8ce660048af9dd7b9bfb2cab28a56
Phua Si Yu Arix <arixgg@gmail.com>
2025-02-22 22:16:38 +0800

    lib: install and setup vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'whiscash.png'],
+      manifest: {
+        name: 'Whiscash',
+        short_name: 'Whiscash',
+        description: 'Expense Tracker',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo-x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve('src/'),
