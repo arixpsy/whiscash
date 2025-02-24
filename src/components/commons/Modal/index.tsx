5f45d2a92efc69bdf63ecc70510c20c00bd14e22
arixpsy <arixgg@gmail.com>
2025-02-25 01:39:16 +0800

    tweak: add modal utils

--- src/components/commons/Modal/index.tsx
+++ src/components/commons/Modal/index.tsx
@@ -2,35 +2,46 @@ import { AnimatePresence, motion } from 'motion/react'
 import { PropsWithChildren, ReactNode, useRef } from 'react'
 import { createPortal } from 'react-dom'
 import { useSearchParams } from 'react-router'
+import { cn } from '@/utils/functions'
 
 type ModalProps = {
   portalKey?: 'route' | 'field'
   paramKey: string
   paramValue: string
+  withoutBackground?: boolean
 } & PropsWithChildren
 
 const Modal = (props: ModalProps) => {
-  const { portalKey, paramKey, paramValue, children } = props
+  const { portalKey, paramKey, paramValue, children, withoutBackground } = props
   const [searchParams] = useSearchParams()
 
   return (
     <AnimatePresence>
       {searchParams.get(paramKey) === paramValue && (
-        <Modal_ portalKey={portalKey}>{children}</Modal_>
+        <Modal_ portalKey={portalKey} withoutBackground={withoutBackground}>
+          {children}
+        </Modal_>
       )}
     </AnimatePresence>
   )
 }
 
-const Modal_ = (props: { portalKey?: string; children: ReactNode }) => {
-  const { portalKey = 'route', children } = props
+const Modal_ = (props: {
+  portalKey?: string
+  children: ReactNode
+  withoutBackground?: boolean
+}) => {
+  const { portalKey = 'route', children, withoutBackground = false } = props
   const isAnimationLocked = useRef(false)
 
   return createPortal(
-    <div className="grid-stack fixed inset-0 place-items-end text-gray-700 ">
+    <div className="grid-stack fixed inset-0 place-items-end justify-items-center text-gray-700">
       <motion.div
         onClick={() => !isAnimationLocked.current && window.history.back()}
-        className="h-full w-full max-w-md bg-black"
+        className={cn(
+          'h-full w-full max-w-md',
+          !withoutBackground && 'bg-black'
+        )}
         initial={{ opacity: 0 }}
         animate={{ opacity: 0.5 }}
         exit={{ opacity: 0 }}
