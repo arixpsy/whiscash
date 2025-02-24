5241b1c1bcebda8e9c2b28949891c851e95e1ec0
arixpsy <arixgg@gmail.com>
2025-02-25 02:05:49 +0800

    wip: add CreateTransactionRequestSchema

--- src/@types/transaction.ts
+++ src/@types/transaction.ts
@@ -1,6 +1,13 @@
+import { Category } from '@/utils/constants/categories'
 import { z } from 'zod'
 
-export const CreateTransactionRequestSchema = z.object({})
+export const CreateTransactionRequestSchema = z.object({
+  walletId: z.number(),
+  amount: z.number(),
+  category: z.nativeEnum(Category),
+  description: z.string(),
+  paidAt: z.string(),
+})
 
 export type CreateTransactionRequest = z.infer<
   typeof CreateTransactionRequestSchema
