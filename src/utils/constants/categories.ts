848697fcf1448e452bc597c5b49c19e1fed1a387
arixpsy <arixgg@gmail.com>
2025-02-27 17:04:04 +0800

    tweak: update category icons

--- src/utils/constants/categories.ts
+++ src/utils/constants/categories.ts
@@ -1,4 +1,9 @@
-import { MdFastfood } from 'react-icons/md'
+import { AiFillInsurance } from 'react-icons/ai'
+import { FaBriefcaseMedical, FaCar } from 'react-icons/fa'
+import { FaBagShopping, FaPlaneDeparture } from 'react-icons/fa6'
+import { GrStatusUnknown } from 'react-icons/gr'
+import { MdFastfood, MdPets, MdWork } from 'react-icons/md'
+import { IoFitness, IoGift } from 'react-icons/io5'
 
 export const Category = {
   Entertainment: 'ENTERTAINMENT',
@@ -20,5 +25,18 @@ export const Category = {
 export type Category = (typeof Category)[keyof typeof Category]
 
 export const CATEGORY_ICON = {
+  [Category.Entertainment]: undefined,
+  [Category.Fitness]: IoFitness,
   [Category.Food]: MdFastfood,
+  [Category.Gifts]: IoGift,
+  [Category.Hobbies]: undefined,
+  [Category.Insurance]: AiFillInsurance,
+  [Category.Medical]: FaBriefcaseMedical,
+  [Category.Others]: GrStatusUnknown,
+  [Category.Pet]: MdPets,
+  [Category.Shopping]: FaBagShopping,
+  [Category.Transport]: FaCar,
+  [Category.Travel]: FaPlaneDeparture,
+  [Category.Utilities]: undefined,
+  [Category.Work]: MdWork,
 }
