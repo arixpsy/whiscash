821e81d8dbf278365209ad996582e5f7f50fd52b
Phua Si Yu Arix <arixgg@gmail.com>
2025-02-27 23:39:16 +0800

    tweak: update Transaction Tile

--- src/components/commons/TransactionTile/index.tsx
+++ src/components/commons/TransactionTile/index.tsx
@@ -1,15 +1,15 @@
-import { Transaction } from '@/@types/shared'
+import { TransactionWithCurrency } from '@/@types/shared'
 import { CATEGORY_ICON } from '@/utils/constants/categories'
 
 type TransactionTileProps = {
-  transaction: Transaction
+  transaction: TransactionWithCurrency
 }
 const TransactionTile = (props: TransactionTileProps) => {
   const { transaction } = props
   const CategoryIcon = CATEGORY_ICON[transaction.category]
 
   return (
-    <div className="mb-2 grid grid-cols-[auto_1fr] gap-3 rounded-lg py-2">
+    <div className="grid grid-cols-[auto_1fr] gap-3 py-2">
       <div className="bg-primary-100 grid h-12 w-12 place-items-center rounded-lg">
         {CategoryIcon && <CategoryIcon className="text-primary-500 h-6 w-6" />}
       </div>
@@ -27,5 +27,22 @@ const TransactionTile = (props: TransactionTileProps) => {
     </div>
   )
 }
+
+const Skeleton = () => (
+  <div className="grid grid-cols-[auto_1fr] gap-3 py-2">
+    <div className="h-12 w-12 animate-pulse rounded-lg bg-gray-200" />
+
+    <div className="flex w-full items-center justify-between">
+      <div className="space-y-1">
+        <div className="h-6 w-24 animate-pulse rounded-lg bg-gray-200" />
+        <div className="h-5 w-18 animate-pulse rounded-lg bg-gray-200" />
+      </div>
+
+      <div className="h-7 w-18 animate-pulse rounded-lg bg-gray-200" />
+    </div>
+  </div>
+)
+
+TransactionTile.Skeleton = Skeleton
 
 export default TransactionTile
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,18 +2,25 @@ import { useMemo, useState } from 'react'
 import { Wallet } from '@/@types/shared'
 import {
   CreateTransactionModal,
+  Loader,
   Page,
+  TransactionTile,
   // TransactionTile,
 } from '@/components/commons'
 import { Header, WalletCarousel } from '@/components/Dashboard'
 import useWallet from '@/hooks/useWallet'
+import useTransaction from '@/hooks/useTransaction'
 
 const Dashboard = () => {
+  const { useGetDashboardWalletTransactionsQuery } = useTransaction()
   const { useGetDashboardWalletsQuery } = useWallet()
   const getDashboardWallets = useGetDashboardWalletsQuery({
     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
   })
   const wallets = getDashboardWallets.data || []
+  const transactions = useGetDashboardWalletTransactionsQuery(
+    wallets.map(({ id }) => ({ walletId: id.toString() }))
+  )
   const [activeIndex, setActiveIndex] = useState<number>(0)
 
   const activeWallet = useMemo<Wallet | undefined>(
@@ -22,6 +29,11 @@ const Dashboard = () => {
         ? getDashboardWallets.data[activeIndex]
         : undefined,
     [activeIndex, getDashboardWallets.data]
+  )
+
+  const activeTransactionQuery = useMemo(
+    () => transactions[activeIndex],
+    [activeIndex, transactions]
   )
 
   return (
@@ -38,6 +50,18 @@ const Dashboard = () => {
 
         <div className="px-3 pb-28">
           <h1 className="mb-3 text-2xl font-bold">Recent Transactions</h1>
+
+          {transactions.length > 0 && activeTransactionQuery.isPending && (
+            <Loader />
+          )}
+
+          <TransactionTile.Skeleton />
+
+          {transactions.length > 0 &&
+            activeTransactionQuery.data &&
+            activeTransactionQuery.data?.map((t) => (
+              <TransactionTile transaction={t} />
+            ))}
         </div>
       </Page>
 
