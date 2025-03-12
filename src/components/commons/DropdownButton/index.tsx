c800d32bd426c1d46485208a32f0494b414297f3
arixpsy <arixgg@gmail.com>
2025-03-12 16:34:45 +0800

    wip: add DropdownButton

---
+++ src/components/commons/DropdownButton/index.tsx
@@ -0,0 +1,120 @@
+import React, {
+  PropsWithChildren,
+  useState,
+  ReactElement,
+  useMemo,
+  ButtonHTMLAttributes,
+  ReactNode,
+  createContext,
+  useContext,
+  HTMLAttributes,
+} from 'react'
+import { AnimatePresence, motion } from 'motion/react'
+import { cn } from '@/utils/functions'
+
+type DropdownButtonContextProps = {
+  isOpen: boolean
+  setIsOpen: (open: boolean) => void
+}
+
+const DropdownButtonContext = createContext<DropdownButtonContextProps>({
+  isOpen: false,
+  setIsOpen: () => {},
+})
+
+const DropdownButton = (props: PropsWithChildren) => {
+  const { children } = props
+  const [isOpen, setIsOpen] = useState(false)
+
+  const { triggerElement, contentElement } = useMemo(() => {
+    let trigger: ReactElement | undefined = undefined
+    let content: ReactElement | undefined = undefined
+    const others: ReactNode[] = []
+
+    React.Children.forEach(children, (child) => {
+      if (!React.isValidElement(child)) return
+      if (child.type === Trigger) trigger = child
+      else if (child.type === Content) content = child
+      else others.push(child)
+    })
+
+    return {
+      triggerElement: trigger,
+      contentElement: content,
+    }
+  }, [children])
+
+  return (
+    <DropdownButtonContext.Provider value={{ isOpen, setIsOpen }}>
+      <div className="relative isolate" style={{ display: 'inherit' }}>
+        {triggerElement}
+
+        {isOpen && (
+          <div className="fixed inset-0" onClick={() => setIsOpen(false)}></div>
+        )}
+
+        {contentElement}
+      </div>
+    </DropdownButtonContext.Provider>
+  )
+}
+
+const Content = (props: HTMLAttributes<HTMLDivElement>) => {
+  const { children, className } = props
+  const context = useContext(DropdownButtonContext)
+
+  if (!context)
+    throw new Error('DropdownButton.Content must be used within DropdownButton')
+
+  const { isOpen } = context
+
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          key="dropdown-menu"
+          className={cn(
+            'absolute top-0 right-0 rounded-lg bg-gray-100 p-3 shadow-lg',
+            className
+          )}
+          initial={{
+            opacity: 0,
+            scaleX: 0,
+            scaleY: 0,
+            originX: 1,
+            originY: 0,
+          }}
+          animate={{
+            opacity: 1,
+            scaleX: 1,
+            scaleY: 1,
+          }}
+          exit={{ opacity: 0, scaleX: 0, scaleY: 0 }}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
+}
+
+const Trigger = (props: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  const { children, className } = props
+  const context = useContext(DropdownButtonContext)
+
+  if (!context)
+    throw new Error('DropdownButton.Trigger must be used within DropdownButton')
+
+  const { setIsOpen } = context
+
+  return (
+    <button type="button" className={className} onClick={() => setIsOpen(true)}>
+      {children}
+    </button>
+  )
+}
+
+DropdownButton.Content = Content
+DropdownButton.Trigger = Trigger
+
+export default DropdownButton
--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,4 +1,6 @@
-import { Page } from '@/components/commons'
+import { DropdownButton, Page } from '@/components/commons'
+import { FaTrash } from 'react-icons/fa'
+import { FiEdit } from 'react-icons/fi'
 import { TbArrowBackUp } from 'react-icons/tb'
 import { TbDotsVertical } from 'react-icons/tb'
 import { useNavigate, useParams } from 'react-router'
@@ -18,6 +20,7 @@ const Transaction = () => {
   const shouldDisplaySkeleton = getTransaction.isPending
   const shouldDisplayTransactionData =
     !getTransaction.isPending && !!transaction
+  const shouldDisplayError = getTransaction.isError
 
   return (
     <Page className="flex flex-col p-3">
@@ -26,15 +29,33 @@ const Transaction = () => {
           <TbArrowBackUp className="h-6 w-6" />
         </button>
 
-        <button type="button">
-          <TbDotsVertical className="h-6 w-6" />
-        </button>
+        <DropdownButton>
+          <DropdownButton.Trigger>
+            <TbDotsVertical className="h-6 w-6" />
+          </DropdownButton.Trigger>
+
+          <DropdownButton.Content className="space-y-3">
+            <button type="button" className="flex items-center gap-3 text-sm">
+              <FiEdit className="h-4 w-4" />
+              Edit
+            </button>
+            <button type="button" className="flex items-center gap-3 text-sm">
+              <FaTrash className="h-4 w-4" />
+              Delete
+            </button>
+          </DropdownButton.Content>
+        </DropdownButton>
       </div>
 
       {shouldDisplaySkeleton && <TransactionDetails.Skeleton />}
 
       {shouldDisplayTransactionData && (
         <TransactionDetails transaction={transaction} />
+      )}
+
+      {shouldDisplayError && (
+        // TODO: add banner
+        <p className="text-center text-red-500"> Error fetching transaction </p>
       )}
     </Page>
   )
