a627febb33ae4de55f1325834b36f78dae2a6c15
arixpsy <arixgg@gmail.com>
2025-03-24 16:52:02 +0800

    tweak: update categories

--- src/utils/constants/categories.ts
+++ src/utils/constants/categories.ts
@@ -1,23 +1,27 @@
 import { AiFillInsurance } from 'react-icons/ai'
 import { BiHappyBeaming, BiSolidMoviePlay } from 'react-icons/bi'
 import { FaBriefcaseMedical, FaCar } from 'react-icons/fa'
-import { FaBagShopping, FaGamepad, FaPlaneDeparture } from 'react-icons/fa6'
+import { FaBagShopping, FaGamepad, FaHouse, FaMoneyBillTransfer, FaPlaneDeparture } from 'react-icons/fa6'
 import { GrStatusUnknown } from 'react-icons/gr'
 import { MdFastfood, MdPets, MdWork } from 'react-icons/md'
 import { IoFitness, IoGift, IoHammer } from 'react-icons/io5'
+import { RiFlowerFill } from 'react-icons/ri'
 
 export const Category = {
+  Accommodation: 'ACCOMMODATION',
   Entertainment: 'ENTERTAINMENT',
   Fitness: 'FITNESS',
   Food: 'FOOD',
   Games: 'GAMES',
   Gifts: 'GIFTS',
+  Grooming: 'GROOMING',
   Hobbies: 'HOBBIES',
   Insurance: 'INSURANCE',
   Medical: 'MEDICAL',
   Others: 'OTHERS',
   Pet: 'PET',
   Shopping: 'SHOPPING',
+  Transfers: 'TRANSFERS',
   Transport: 'TRANSPORT',
   Travel: 'TRAVEL',
   Utilities: 'UTILITIES',
@@ -27,17 +31,20 @@ export const Category = {
 export type Category = (typeof Category)[keyof typeof Category]
 
 export const CATEGORY_ICON = {
+  [Category.Accommodation]: FaHouse,
   [Category.Entertainment]: BiSolidMoviePlay,
   [Category.Fitness]: IoFitness,
   [Category.Food]: MdFastfood,
   [Category.Games]: FaGamepad,
   [Category.Gifts]: IoGift,
+  [Category.Grooming]: RiFlowerFill,
   [Category.Hobbies]: BiHappyBeaming,
   [Category.Insurance]: AiFillInsurance,
   [Category.Medical]: FaBriefcaseMedical,
   [Category.Others]: GrStatusUnknown,
   [Category.Pet]: MdPets,
   [Category.Shopping]: FaBagShopping,
+  [Category.Transfers]: FaMoneyBillTransfer,
   [Category.Transport]: FaCar,
   [Category.Travel]: FaPlaneDeparture,
   [Category.Utilities]: IoHammer,
