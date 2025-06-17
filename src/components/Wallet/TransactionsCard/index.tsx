1838bd190e6c05ba7892e0397c772e287402605f
arixpsy <arixgg@gmail.com>
2025-06-17 16:57:44 +0800

    tweak: add nav onClick to transactionTile in wallet page

--- src/components/Wallet/TransactionsCard/index.tsx
+++ src/components/Wallet/TransactionsCard/index.tsx
@@ -1,5 +1,7 @@
 import { Transaction, Wallet } from '@/@types/shared'
 import { TransactionTile } from '@/components/commons'
+import { Route } from '@/utils/constants/routes'
+import { useNavigate } from 'react-router'
 
 type TransactionsCardProps = {
   data: Array<Transaction>
@@ -10,23 +12,35 @@ type TransactionsCardProps = {
 
 const TransactionsCard = (props: TransactionsCardProps) => {
   const { data, wallet } = props
+  const navigate = useNavigate()
+
+  const handleNavigateToTransaction = (transactionId: number) =>
+    document.startViewTransition(() =>
+      navigate(`${Route.TRANSACTIONS}/${transactionId}`, {
+        state: { from: Route.WALLETS + '/' + wallet.id },
+      })
+    )
 
   return (
-    <div className='py-3'>
-      <p className="mb-1 text-lg font-bold px-3">Transactions</p>
+    <div className="py-3">
+      <p className="mb-1 px-3 text-lg font-bold">Transactions</p>
       <div className="rounded-lg bg-white">
-        {data.slice().reverse().map((r) => (
-          <TransactionTile
-            key={r.id}
-            transaction={{
-              ...r,
-              name: wallet.name,
-              currency: wallet.currency,
-              country: wallet.country,
-              subWalletOf: wallet.subWalletOf,
-            }}
-          />
-        ))}
+        {data
+          .slice()
+          .reverse()
+          .map((r) => (
+            <TransactionTile
+              key={r.id}
+              transaction={{
+                ...r,
+                name: wallet.name,
+                currency: wallet.currency,
+                country: wallet.country,
+                subWalletOf: wallet.subWalletOf,
+              }}
+              onClick={() => handleNavigateToTransaction(r.id)}
+            />
+          ))}
       </div>
     </div>
   )
